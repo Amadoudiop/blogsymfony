6473f6cec811ee242fb0ad4a3d83a8e00b93837d
lucascadrecha <lucascadrecha@gmail.com>
2018-10-12 12:00:51 +0200

    création article

---
+++ src/AppBundle/Form/ArticleType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date_create',DateType::class)
+            ->add('date_update',DateType::class)
+            ->add('date_event',DateType::class)
+            ->add('title',TextType::class)
+            ->add('catch_sentence',TextType::class)
+            ->add('picture', FileType::class, array('label' => 'picture (PDF file)'))
+            ->add('content',TextareaType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Article'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_article';
+    }
+
+
+}
